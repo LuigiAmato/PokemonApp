609b9360a1c1577fd8d803cf07949b31122f48b4
Amato, Luigi <luigi.amato@nttdata.com>
2023-01-27 14:33:32 +0100

    checkout project

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Amato, Luigi on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
